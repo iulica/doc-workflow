f6e762d99f5777477fe18d1a247ea163b760ca64
Iulian Ciorascu <iulica@iclic.ro>
2022-04-01 19:51:52 +0200

    added readme to setup

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from distutils.core import setup
 setup(
   name='doc-workflow',
   packages=['docwf'],
-  version='0.1.0-alpha3',
+  version='0.1.0-alpha4',
   license='MIT',
   description='A Python Document Management Framework for generating and sending (pdf, docx, etc) documents to customers',
+  long_description=open('README.md').read(),
   author='Iulian Ciorăscu',
   author_email='ciulian@gmail.com',
   url='https://github.com/iulica/doc-workflow',
